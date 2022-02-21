ff5ffeeb8358e31d7ce710ca6114459cf6119dd4
Dayana Esquivel <desquivel@geniuscode.mx>
2022-02-21 12:43:35 -0700

    add: Notebook

---
+++ Kata1/documentacion1.py
@@ -0,0 +1,27 @@
+#sum = 1 + 2 #3
+#product = sum * 2
+#print(product)
+
+# Declaramos la variable
+#distancia_a_alfa_centauri = 4.367
+
+# Descubrimos su tipo de dato
+#print(type(distancia_a_alfa_centauri))
+
+
+# Importamos la biblioteca 
+from datetime import date
+
+# Obtenemos la fecha de hoy
+date.today()
+
+# Mostramos la fecha en la consola
+#print("Today's date is: " + str(date.today()))
+#print("Bienvenido al programa de bienvenida")
+#name = input("Introduzca su nombre ")
+#print("Saludos: " + name)
+
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(int(first_number) + int(second_number))
